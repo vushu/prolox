93085a1fc6d8afa1820dd9ef39c09ea61118f79d
davu <davu@mir-robots.com>
2024-09-08 23:15:19 +0200

    added parser

--- prolog/prolox/main.pl
+++ prolog/prolox/main.pl
@@ -1,5 +1,9 @@
 :- use_module(scanner).
+:- use_module(parser).
 
 main :-
-	scan("2+2=", Tokens), 
-	format("Tokens: ~w", Tokens).
+	scan("2*4>=2 != 4 or 1 and (2+2); a = 2;", Tokens), 
+	% scan("a = (2 - 2);", Tokens), 
+	% writeln(Tokens),
+	parse(Tokens, Exprs),
+	writeln(Exprs).
---
+++ prolog/prolox/parser.pl
@@ -0,0 +1,152 @@
+:- module(parser,[parse/2]).
+
+parse(Tokens, Exprs) :-
+	phrase(
+		parse_expr(Exprs), Tokens).
+
+parse_expr([Expr|Rest])-->
+	expression_stmt(Expr), 
+	parse_expr(Rest).
+
+parse_expr([])-->
+	[].
+	% {throw("Failed to parse")}, [].
+
+expression_stmt(Expr)-->
+	expression(Expr), 
+	[token(semicolon, _)].
+
+expression_stmt(_)-->
+	expression(_), 
+	
+	{throw("Expected ';' after expression.")}, [].
+
+expression(Expr)-->
+	assignment_expr(Expr).
+
+%Unary expr
+
+term_op(Op)-->
+	[T], 
+	
+	{T = token(Op, _), 
+	memberchk(Op, [plus, minus])}.
+
+factor_op(Op)-->
+	[T], 
+	
+	{T = token(Op, _), 
+	memberchk(Op, [slash, star])}.
+
+unary_op(T)-->
+	[T], 
+	
+	{T = token(Op, _), 
+	memberchk(Op, [bang, minus])}.
+
+comparison_op(T)-->
+	[T], 
+	
+	{T = token(Op, _), 
+	memberchk(Op, [greater, greater_equal, less, less_equal])}.
+
+equality_op(Op)-->
+	[T], 
+	
+	{T = token(Op, _), 
+	memberchk(Op, [bang_equal, equal_equal])}.
+
+assignment_expr(E)-->
+	or_expr(E).
+
+assignment_expr(assigment(assign_name(E), value(E2)))-->
+	or_expr(E), 
+	[token(equal, _)], 
+	assignment_expr(E2).
+
+or_expr(E)-->
+	and_expr(E).
+
+or_expr(and(left(E), right(E2), op(T)))-->
+	and_expr(E), [T], 
+	
+	{T = token(or, _)}, 
+	and_expr(E2).
+
+and_expr(E)-->
+	equality_expr(E).
+
+and_expr(and(left(E), right(E2), op(T)))-->
+	equality_expr(E), [T], 
+	
+	{T = token(and, _)}, 
+	equality_expr(E2).
+
+equality_expr(E)-->
+	comparison_expr(E).
+
+equality_expr(equality(left(E), right(E2), op(Op)))-->
+	comparison_expr(E), 
+	equality_op(Op), 
+	comparison_expr(E2).
+
+comparison_expr(E)-->
+	term_expr(E).
+
+comparison_expr(comparison(left(E), right(E2), op(Op)))-->
+	term_expr(E), 
+	comparison_op(Op), 
+	term_expr(E2).
+
+term_expr(E)-->
+	factor_expr(E).
+
+term_expr(term(left(E), right(E2), op(Op)))-->
+	factor_expr(E), 
+	term_op(Op), 
+	factor_expr(E2).
+
+factor_expr(E)-->
+	unary_expr(E).
+
+factor_expr(factor(left(E), right(E2), op(Op)))-->
+	unary_expr(E), 
+	factor_op(Op), 
+	unary_expr(E2).
+
+unary_expr(E)-->
+	primary_expr(E).
+
+unary_expr(unary(op(Op), right(E)))-->
+	unary_op(Op), 
+	unary_expr(E). 
+
+primary_expr(primary(number(Num)))-->
+	[token(
+		number(Num), _)].
+
+primary_expr(primary(string(X)))-->
+	[token(
+		string(X), _)].
+
+primary_expr(primary(true))-->
+	[token(true, _)].
+
+primary_expr(primary(false))-->
+	[token(false, _)].
+
+primary_expr(primary(nil))-->
+	[token(nil, _)].
+
+primary_expr(primary(T))-->
+	[T], 
+	
+	{T = token(
+		identifier(_), _)}.
+
+primary_expr(primary(group(E)))-->
+	[token(left_paren, _)], 
+	expression(E), 
+	[token(right_paren, _)].
+
+
