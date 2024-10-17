68254d3120fcad49b3c50f91e15d1c2d8a135c89
davu <davu@mir-robots.com>
2024-10-17 15:36:58 +0200

    adding env to interpreter

--- prolog/prolox/interpreter.pl
+++ prolog/prolox/interpreter.pl
@@ -2,97 +2,95 @@
 :- use_module(environment).
 
 interpret([Stmt|Stmts]) :-
-	C = context(
-		res(_), _), 
-	evaluate(Stmt, C), 
-	evaluate_rest(Stmts, C).
+	create_new_env(Env), 
+	evaluate(Stmt, Env, C), 
+	evaluate_rest(Stmts, Env, C).
 
-evaluate_rest([Stmt|Stmts], C) :-
-	evaluate(Stmt, C), 
-	evaluate_rest(Stmts, C).
+evaluate_rest([Stmt|Stmts], Env, C) :-
+	evaluate(Stmt, Env, C), 
+	evaluate_rest(Stmts, Env, C).
 
-evaluate_rest([], _).
+evaluate_rest([],_, _).
 
-evaluate(print(X), _) :-
-	evaluate(X, R), 
+evaluate(print(X), Env, _) :-
+	evaluate(X, Env, R), 
 	format("printing: ~w~n", R), !.
 
-evaluate(primary(true), true).
+evaluate(primary(true),_, true).
 
-evaluate(primary(false), false).
+evaluate(primary(false),_, false).
 
-evaluate(primary(nil), nil).
+evaluate(primary(nil),_, nil).
 
-evaluate(primary(X), Y) :-
-	evaluate(X, Y).
+evaluate(primary(X), Env, Y) :-
+	evaluate(X, Env, Y).
 
 %bool and nil
 
-evaluate(number(X), X).
-evaluate(string(X), X).
+evaluate(number(X),_, X).
+evaluate(string(X),_, X).
 
-evaluate(expr_stmt(S), R) :-
-	evaluate(S, R).
+evaluate(expr_stmt(S), Env, R) :-
+	evaluate(S, Env, R).
 
-evaluate(factor(left(L), right(R), op(Op)), Res) :-
-	evaluate(L, L2), 
-	evaluate(R, R2), 
+evaluate(factor(left(L), right(R), op(Op)), Env, Res) :-
+	evaluate(L, Env, L2), 
+	evaluate(R, Env, R2), 
 	(Op = star, Res is L2*R2;
 	Res is L2/R2).
 
-evaluate(term(left(L), right(R), op(Op)), Res) :-
-	evaluate(L, L2), 
-	evaluate(R, R2), 
+evaluate(term(left(L), right(R), op(Op)), Env, Res) :-
+	evaluate(L, Env, L2), 
+	evaluate(R, Env, R2), 
 	(Op = plus, Res is L2 + R2;
 	Res is L2 - R2).
 
-evaluate(or(left(L), right(R), op(token(or, _))), Res) :-
-	evaluate(L, L2), 
+evaluate(or(left(L), right(R), op(token(or, _))), Env, Res) :-
+	evaluate(L, Env, L2), 
 	is_truthy(L2, T), 
-	evaluate(R, R2), 
+	evaluate(R, Env, R2), 
 	is_truthy(R2, T2), 
 	(T = true, Res = L2;
 	T2 = true, Res = R2;
 	Res = false).
 
-evaluate(and(left(L), right(R), op(token(and, _))), Res) :-
-	evaluate(L, L2), 
+evaluate(and(left(L), right(R), op(token(and, _))), Env, Res) :-
+	evaluate(L, Env, L2), 
 	is_truthy(L2, T), 
-	evaluate(R, R2), 
+	evaluate(R, Env, R2), 
 	is_truthy(R2, T2), 
 	(T = true, T2 = true, Res = true;
 	Res = false).
 
-evaluate(comparison(left(L), right(R), op(token(Op, _))), Res) :-
-	evaluate(L, L2), 
-	evaluate(R, R2), 
+evaluate(comparison(left(L), right(R), op(token(Op, _))), Env, Res) :-
+	evaluate(L, Env, L2), 
+	evaluate(R, Env, R2), 
 	(Op = greater, L2 > R2, Res = true;
 	Op = greater_equal, L2 >= R2, Res = true;
 	Op = less, L2 < R2, Res = true;
 	Op = less_equal, L2 =< R2, Res = true;
 	Res = false).
 
-evaluate(equality(left(L), right(R), op(token(Op, _))), Res) :-
-	evaluate(L, L2), 
-	evaluate(R, R2), 
+evaluate(equality(left(L), right(R), op(token(Op, _))), Env, Res) :-
+	evaluate(L, Env, L2), 
+	evaluate(R, Env, R2), 
 	(Op = bang_equal, 
 		not(L2 = R2), Res = true;
 		Op = equal_equal, L2 == R2, Res = true;
 		Res = false).
 
-evaluate(group(E), R) :-
-	evaluate(E, R).
+evaluate(group(E), Env, R) :-
+	evaluate(E, Env, R).
 
-evaluate(unary(op(token(Op, _)), right(E)), R) :-
-	evaluate(E, E2), 
+evaluate(unary(op(token(Op, _)), right(E)), Env, R) :-
+	evaluate(E, Env, E2), 
 	(Op = bang, 
 		negate(E2, R);
 		Op = minus, R is  - E2;
 		format("Op is unknown: ~w~n", Op), !).
 
-evaluate(expr_stmt(Expr), Env) :-
-	create_new_env(Env), 
-	evaluate(Expr, Env).
+evaluate(expr_stmt(Expr), Env, R) :-
+	evaluate(Expr, Env, R).
 
 % evaluate(block([Stmt|Stmts]), S) :-
 % 	evaluate(Stmt, S), 
@@ -102,10 +100,9 @@ evaluate(expr_stmt(Expr), Env) :-
 % 	evaluate(Stmt, S), 
 % 	evaluate_block_rest(Stmts, S).
 
-% evaluate_block_rest([], _).
+% evaluate_block_rest([], _, _).
 
-
-evaluate(_, _) :-
+evaluate(_, _, _) :-
 	writeln("Unknown stmt"), halt.
 
 % block_stmt(block(Stmts))-->
