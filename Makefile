69f44442bf4cdcbae8bf6bc0ae2e9edf971d1185
davu <davu@mir-robots.com>
2024-10-29 08:01:00 +0100

    updated make and gitignore

--- Makefile
+++ Makefile
@@ -13,7 +13,7 @@ test-interpreter:
 	swipl -s tests/interpreter_test.pl -g run_tests,halt -t 'halt(1)'
 
 main:
-	swipl -o prolox -g main -c prolog/prolox/main.pl
+	swipl -o run_prolox -g main -c prolog/prolox/main.pl
 
 clean:
-	rm -f prolox
+	rm -f run_prolox
--- prolog/prolox/main.pl
+++ prolog/prolox/main.pl
@@ -3,7 +3,6 @@
 :- use_module(interpreter).
 
 main :-
-	scan("var foo = 0; foo = 42;", Tokens), 
-	% scan("print 1 + 1;", Tokens), 
-	parse(Tokens, Stmts), 
-	interpret(Stmts), nl, fail.
+	scan("var foo = 0; foo = 42; print foo;", Tokens), % scan("print 1 + 1;", Tokens), 
+	parse(Tokens, Stmts),
+	interpret(Stmts).
